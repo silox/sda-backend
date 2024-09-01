aadc8d951d462f1346b92434c851988b65f0e2f0
silox <edruisl99@gmail.com>
2024-09-01 13:20:59 +0200

    Add movie permissions

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,6 @@
 import logging
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -39,15 +39,13 @@ class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'profile.html'
 
     def get_context_data(self, **kwargs):
-        print(dir(self.request.user))
-        print(self.request.user.profile)
-        print(Profile.objects.all())
         return {**super().get_context_data(), 'object': self.request.user}
 
 
-class MovieListView(LoginRequiredMixin, ListView):
+class MovieListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     template_name = 'movie_list.html'
     model = Movie
+    permission_required = 'viewer.view_movie'
 
     def get_context_data(self, **kwargs):
         movies = Movie.objects.all()
@@ -57,25 +55,28 @@ class MovieListView(LoginRequiredMixin, ListView):
         }
 
 
-class MovieDetailView(LoginRequiredMixin, DetailView):
+class MovieDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'movie_detail.html'
     model = Movie
+    permission_required = 'viewer.view_movie'
 
 
-class MovieCreateView(LoginRequiredMixin, CreateView):
+class MovieCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     template_name = 'movie_form.html'
     form_class = MovieForm
     success_url = reverse_lazy('viewer:movie_list')
+    permission_required = 'viewer.add_movie'
 
     def form_invalid(self, form):
         logger.warning('User provided invalid data.')
         return super().form_invalid(form)
 
 
-class MovieUpdateView(LoginRequiredMixin, UpdateView):
+class MovieUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     template_name = 'movie_form.html'
     model = Movie
     form_class = MovieForm
+    permission_required = 'viewer.change_movie'
 
     def form_invalid(self, form):
         logger.warning('User provided invalid data while updating a movie.')
@@ -90,11 +91,12 @@ class MovieUpdateView(LoginRequiredMixin, UpdateView):
         assert False, 'Unexpectedly got no _save or _continue in request.POST'
 
 
-class MovieDeleteView(LoginRequiredMixin, DeleteView):
+class MovieDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     template_name = 'obj_delete_form.html'
     model = Movie
     success_url = reverse_lazy('viewer:movie_list')
     extra_context = {'model_name': 'movie'}
+    permission_required = 'viewer.delete_movie'
 
 
 class GenreListView(LoginRequiredMixin, ListView):
