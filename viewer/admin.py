b9a623123eb0eb8b3d19f47efe27e0a77d277489
silox <edruisl99@gmail.com>
2024-09-06 11:22:41 +0200

    Add movie admin

--- viewer/admin.py
+++ viewer/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 
 from viewer.models import Genre, Movie
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    @staticmethod
+    def released_year(obj):
+        return obj.released.year
+
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        queryset.update(description='')
+
+    ordering = ['id']
+    list_display = ['id', 'title', 'genre', 'released_year']
+    list_display_links = ['id', 'title']
+    list_per_page = 20
+    list_filter = ['genre']
+    search_fields = ['title', 'genre__name']
+    actions = ['cleanup_description']
+
+
 admin.site.register(Genre)
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
