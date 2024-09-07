e4115e53dad70a30e06bd250e0762948dce063f8
silox <edruisl99@gmail.com>
2024-09-07 13:47:25 +0200

    Add truncate template filter

--- viewer/templatetags/tt_utils.py
+++ viewer/templatetags/tt_utils.py
@@ -28,7 +28,14 @@ def to_nice_date(date_param):
     return f'In {days_num} days'
 
 
+def truncate_filter(text, max_len=12):
+    if max_len <= 0:
+        raise ValueError('max_len must be > 0')
+    if max_len <= 3:
+        return text[:max_len]
+    return text if len(text) <= max_len else f'{text[:max_len - 3]}...'
 
 
 register.simple_tag(cut, name='cut')
 register.filter("to_nice_date", to_nice_date)
+register.filter('truncate', truncate_filter)
